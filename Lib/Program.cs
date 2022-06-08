56c3b444815bab28e90d15ddc92cc7473d5d74c3
pavel <pavel@DESKTOP-EDGG59N>
2022-06-08 17:08:11 +0300

    Add project files.

---
+++ Lib/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Convert;
+using WorldOfTanks;
+
+namespace Lib
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ch;
+            WriteLine("Сколько такнов будет играть ?");
+            ch = ToInt32(ReadLine());
+             
+            Tank[] T34 = new Tank[ch];
+            Tank[] Pantera = new Tank[ch];
+            for (int i = 0; i < ch; i++) T34[i] = new Tank("T34");
+            for (int i = 0; i < ch; i++) Pantera[i] = new Tank("Pantera");
+
+            for (int i = 0; i < ch; i++)
+            {
+                string s = "";
+                int t1 = T34[i];
+                int t2 = Pantera[i];
+                if (t1 > t2)
+                    WriteLine($"Бой номер {i + 1}. Победил танк {T34[i].name} \nХарактеристики: \nБроня - {T34[i].bronya}\nПатроны - {T34[i].patron}\nМаневреность - {T34[i].manevrenost}\n"); 
+                else 
+                    WriteLine($"Бой номер {i + 1}. Победил танк {Pantera[i].name}\nХарактеристики: \nБроня - {T34[i].bronya}\nПатроны - {T34[i].patron}\nМаневреность - {T34[i].manevrenost}\n");
+                Thread.Sleep(1000);
+            } 
+        }
+    }
+}
---
+++ WorldOfTanks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Convert;
+
+namespace WorldOfTanks
+{ 
+    public class Tank
+    {
+        static void Main()
+        {  }
+
+        public string name { get; set; }
+        public int bronya { get; set; }
+        public int patron { get; set; }
+        public int manevrenost { get; set; }
+         
+        public Tank(string n)
+        {
+            Random r = new Random();
+            name = n;
+            bronya = r.Next(0, 100);
+            patron = r.Next(0, 100);
+            manevrenost = r.Next(0, 100);
+        } 
+
+        public static implicit operator int(Tank t)
+        {
+            return t.bronya * t.patron;
+        }
+    } 
+}
